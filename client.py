9bde4361d57635d93935116e00ab4a9a6c6318f3
THESPLITTER04\bramd <bramdedrick@gmail.com>
2021-03-02 20:08:17 -0500

    Implemented basic client python code.

---
+++ client.py
@@ -0,0 +1,60 @@
+import socket
+import time
+
+class MessagingClient:
+    def __init__(self): #Initializes Client
+        self.name = socket.gethostname()
+        self.ip_address = socket.gethostbyname(self.name)
+
+        self.port = 50001
+
+        self.client_addr = (self.ip_address, self.port)
+
+    def connect(self, server_ip):
+        self.server_addr = (server_ip, self.port)
+
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect(self.server_addr)
+
+    def message(self, messsage):
+        encodeMsg = message.encode("utf-8")
+
+        bufsize = len(encodeMsg)
+
+        self.client_socket.send(str(bufsize).encode("utf-8"))
+
+        time.sleep(1)
+
+        conf = self.client_socket.recv(8)
+
+        self.client_socket.send(encodeMsg)
+
+    def recieve(self):
+        responseSize = self.client_socket.recv(4).decode("utf-8")
+        responseSizeStr = str(responseSize)
+        responseSizeInt = int(responseSizeStr)
+        response = client_socket.recv(responseSizeInt).decode("utf-8")
+
+        print(response) #For Testing
+        return response
+
+    def diconnect(self):
+        self.client_socket.close()
+
+def main():
+
+    clientSocket = MessagingClient()
+    clientSocket.connect("10.104.65.5")
+    clientSocket.message("test")
+
+    control = True
+    while control:
+        time.sleep(1)
+        response = clientSocket.recieve()
+        if response == "%end":
+            control = False
+
+    clientSocket.close()
+
+if __name__ == "__main__":
+    main()
